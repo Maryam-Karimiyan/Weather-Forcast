457da231d804a244a14628b1ef833ddc24312f2f
Maryam-Karimiyan <92780215+Maryam-Karimiyan@users.noreply.github.com>
2022-03-02 14:04:50 +0330

    commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
+//this file is for dom manipulation
 const cityForm=document.querySelector('form');
+
 const updateCity=async (city)=>{
-console.log(city);
+const cityDetails=await getCity(city);
+const weather=await getWeather(cityDetails.Key);
+return {
+    cityDets:cityDetails,
+    weather:weather
+    //cause they're the same name we can just type only one of them like weather
+}
 }
 cityForm.addEventListener('submit',e=>{
     e.preventDefault();
@@ -9,5 +17,11 @@ cityForm.addEventListener('submit',e=>{
     cityForm.reset();
 
     //update ui whith new city
-updateCity(city);
+updateCity(city)
+.then(data=>{
+    console.log(data);
+})
+.catch(err=>{
+    console.log(err);
+});
 })
--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,31 +1,38 @@
+//this file is for interacting with apis
 const key = "vEZ5aEnLpxLg2UuO9cMizBufOszvltgj";
 
 //get weather information
-const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
- // const query = `${id}?apikey=${key}`;
-  const response = await fetch(base+`${id}?apikey=${key}`);
-  const data = await response.json();
- return data[0];
+const getWeather = async (key_id) => {
+  try {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${key_id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+    
+  } catch (err) {
+    alert(err);
+  }
 };
-
-
 
 //get city information
 const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + query);
-  const data = await response.json();
-  return data[0];
+  try {
+    const base =
+      "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+  } catch (err) {
+    alert(err);
+  }
 };
-
 getCity("manchester")
-  .then(data => {
-   return getWeather(data.key)
+  .then((data) => {
+    return getWeather(data.Key);
   })
-  .then(data=>{
-      console.log(data);
+  .then((data) => {
+    console.log(data);
   })
   .catch((err) => console.log(err));
-//329260
